cb5a421db81e2ecb13ecc874d5d2b800da9d5a99
Ilkka Laukkanen <ext-ilkka.laukkanen@nokia.com>
2010-11-19 18:13:04 +0200

    Configure watch file to seek in PyPi

--- debian/watch.ex
+++ debian/watch.ex
@@ -1,23 +1,8 @@
-# Example watch control file for uscan
-# Rename this file to "watch" and then you can run the "uscan" command
-# to check for upstream updates and more.
-# See uscan(1) for format
+# Watch control file for uscan. You can run the "uscan" command to check for
+# upstream updates and more. See uscan(1) for format
 
 # Compulsory line, this is a version 3 file
 version=3
 
-# Uncomment to examine a Webpage
-# <Webpage URL> <string match>
-#http://www.example.com/downloads.php python-enum-(.*)\.tar\.gz
-
-# Uncomment to examine a Webserver directory
-#http://www.example.com/pub/python-enum-(.*)\.tar\.gz
-
-# Uncommment to examine a FTP server
-#ftp://ftp.example.com/pub/python-enum-(.*)\.tar\.gz debian uupdate
-
-# Uncomment to find new files on sourceforge, for devscripts >= 2.9
-# http://sf.net/python-enum/python-enum-(.*)\.tar\.gz
-
-# Uncomment to find new files on GooglePages
-# http://example.googlepages.com/foo.html python-enum-(.*)\.tar\.gz
+# Check PyPi page
+http://pypi.python.org/pypi/enum/ enum-(.*)\.tar\.gz
